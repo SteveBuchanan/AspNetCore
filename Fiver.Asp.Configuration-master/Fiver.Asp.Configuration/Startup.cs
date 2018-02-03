a27e20ffe3169196d8e6f07c010bc4043f0f8347
Steve Buchanan <Steve_Buchanan@Hotmail.com>
2018-02-03 12:19:55 -0500

    Add files via upload
    This is  using middleware and options

---
+++ Fiver.Asp.Configuration-master/Fiver.Asp.Configuration/Startup.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Fiver.Asp.Configuration
+{
+    public class Startup
+    {
+        public static IConfiguration Config { get; private set; }
+
+        public Startup(
+            IConfiguration config)
+        {
+            Config = config;
+        }
+
+        public void ConfigureServices(
+            IServiceCollection services)
+        {
+            services.AddOptions();
+            services.Configure<AppSettings>(Config);
+        }
+
+        public void Configure(
+            IApplicationBuilder app, 
+            IHostingEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseHelloWorld();
+        }
+    }
+}

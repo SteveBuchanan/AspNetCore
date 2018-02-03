c2c9384fcef1e5cb333ee8bda5885025a2096cac
Steve Buchanan <Steve_Buchanan@Hotmail.com>
2018-02-03 10:51:24 -0500

    Add files via upload

---
+++ DotNetCore20Config/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DotNetCore20Config.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace DotNetCore20Config.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public HomeController(IConfiguration Configuration)
+        {
+            _configuration = Configuration;
+        }
+
+        public IActionResult Index()
+        {
+            // Read configuration using Key string
+            ViewData["MySectionMyFirstConfig"] = _configuration["MySection:MyFirstConfig"];
+
+            // Read configuration using GetSection method
+            ViewData["MySectionMySecondConfigMyFirstSubConfig"] = _configuration.GetSection("MySection").GetSection("MySecondConfig").GetSection("MyFirstSubConfig");
+
+            // Read configuration using mixed options with GetSection method and Key string
+            ViewData["MySectionMySecondConfigMySecondSubConfig"] = _configuration.GetSection("MySection")["MySecondConfig:MySecondSubConfig"];
+
+            ViewData["MyConnectionString"] = _configuration.GetConnectionString("localDB");
+            return View();
+
+
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DotNetCore20Config/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DotNetCore20Config.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
